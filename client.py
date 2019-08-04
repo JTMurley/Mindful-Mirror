318747e50e7675e16a23eb5a626646a9308dbf2c
kaizhilam <kaizhilam@gmail.com>
2019-08-05 09:52:50 +1000

    Added JSON to GUI functionality. Modify the json file. Press Enter to Update the GUI live

--- client.py
+++ client.py
@@ -1,21 +1,61 @@
 from tkinter import *
+import json
 
 class Mirror():
     """UI for the mirror"""
     
+    UIElements:list = []
+
     def __init__(self, screenWidth = None, screenHeight = None, backgroundColour = "black"):
         self.ScreenWidth = screenWidth
         self.ScreenHeight = screenHeight
         self.BackgroundColour = backgroundColour
-        root = Tk()
+        self.root = Tk()
+        root = self.root
         if self.ScreenWidth == None: self.ScreenWidth = root.winfo_screenwidth()
         if self.ScreenHeight == None: self.ScreenHeight = root.winfo_screenheight()
         root.geometry(str(self.ScreenWidth)+"x"+str(self.ScreenHeight))
         root.resizable(width = FALSE, height = FALSE)
         root.config(bg = self.BackgroundColour, cursor = "None")
         root.bind("<Escape>", self.Shutdown) #binds ESC key to shut down mirror
-        root.overrideredirect(True) #remove title bar
+        root.bind("<Return>", self.__Update) #Update widget for test purpose
+        #root.overrideredirect(True) #remove title bar
+        self.__Populate()
         root.mainloop()
+    
+    def __Populate(self):
+        """Populating elements inside the Tkinter GUI with clientgui.JSON"""
+
+        with open("clientgui.json") as file:
+            data = json.load(file)
+        for i in data:
+            if i["x"] < 0:
+                x = self.root.winfo_screenwidth()+i["x"]
+            else:
+                x = i["x"]
+            if i["y"] < 0:
+                y = self.root.winfo_screenheight()+i["y"]
+            else:
+                y = i["y"]
+            if i["type"] == "label":
+                item = Label(self.root, 
+                text = i["text"],
+                font = (i["font"], i["font size"]),
+                fg=i["font colour"],
+                width = i["width"], 
+                height = i["height"],
+                bg=self.BackgroundColour)
+                item.place(x = x, y = y)
+                self.UIElements.append(item)
+
+    def __Update(self, e):
+        """Updating all the element from the JSON file"""
+        
+        for i in self.UIElements:
+            i.destroy()
+        self.UIElements.clear()
+        print(self.UIElements)
+        self.__Populate()
 
     def Shutdown(self, e):
         exit()
